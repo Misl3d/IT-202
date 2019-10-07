7baee4b1f4966eb0d52e6291a7b8271e3212e05a
Misl3d <thisismisled@gmail.com>
2019-10-07 10:53:38 -0400

    Created form.php

---
+++ form.php
@@ -0,0 +1,53 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function getName()
+{
+	if(isset($_POST['name']))
+	{
+		$name = $_POST['name'];
+		echo "<p>Hello, " . $name . "</p>";
+		echo "<br>";
+	}
+}
+?>
+
+<html>
+<head>
+	<script>
+		function checkPassword(form)
+		{   	
+			if (form.password.value != form.confirm_password.value)
+			{ 
+                    		alert ("\nPasswords do not match: try again...") 
+                    		return false;
+			} 
+  
+                	else
+			{ 
+                    		return true; 
+                	}
+		}
+	</script>
+</head>
+<body>
+	<?php getName();?>
+	<form method="POST" action="#" onSubmit="return checkPassword(this)">
+		<input name="name" type="text" placeholder="username"/>
+		<input name="password" type="password" id="password" placeholder="password"/>
+		<input name="confirm_password" type="password" id="confirm_password" placeholder="confirm password"/>
+
+		<input type="submit" value="Try it"/>
+	</form>
+	<br><a href="">Github</a><br>
+</body>
+</html>
+
+<?php
+if(isset($_POST))
+{
+	echo "<br><pre>" . var_export($_POST, true) . "</pre><br>";
+}
+?>
